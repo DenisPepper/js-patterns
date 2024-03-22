e7208047437c7a1b33fde7ec54583e9092939906
DenisPepper <thech@yandex.ru>
2024-03-22 11:39:29 +0300

    Добавит пример использования

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,4 +1,3 @@
-/*
 function Pipeline(...middlewares) {
     const stack = middlewares
   
@@ -30,4 +29,39 @@ function Pipeline(...middlewares) {
   
     return { push, execute }
   }
-  */
+  
+
+  // create a middleware pipeline
+const pipeline = Pipeline(
+    // with an initial middleware
+    (ctx, next) => {
+      console.log(ctx)
+      next()
+    }
+  )
+  
+  // add some more middlewares
+  pipeline.push(
+    (ctx, next) => {
+      ctx.value = ctx.value + 21
+      next()
+    },
+    (ctx, next) => {
+      ctx.value = ctx.value * 2
+      next()
+    }
+  )
+  
+  // add the terminating middleware
+  pipeline.push((ctx, next) => {
+    console.log(ctx)
+    // not calling `next()`
+  })
+  
+  // add another one for fun ¯\_(ツ)_/¯
+  pipeline.push((ctx, next) => {
+    console.log('this will not be logged')
+  })
+  
+  // execute the pipeline with initial value of `ctx`
+  pipeline.execute({ value: 0 })
