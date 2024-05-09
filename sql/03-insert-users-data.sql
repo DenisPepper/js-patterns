6d866758ec0a9c6c2b00fce2b8cda52f6bb38ed7
DenisPepper <thech@yandex.ru>
2024-05-09 13:14:30 +0300

    Добавит строки в таблицу users

--- sql/03-insert-users-data.sql
+++ sql/03-insert-users-data.sql
@@ -1,2 +1,8 @@
+--INSERT INTO users (current_status, salary, full_name) 
+--VALUES ('employed', 20000, 'Denis Pepper' );
+
 INSERT INTO users (current_status, salary, full_name) 
-VALUES ('employed', 20000, 'Denis Pepper' );
+VALUES ('employed', 25000, 'Иванов Иван Иванович' );
+
+INSERT INTO users (current_status, salary, full_name) 
+VALUES ('self-employed', 30000, 'Петров Петр Петрович' );
