2f1fe76534369917219674f2e39a242ce5e460dc
DenisPepper <thech@yandex.ru>
2024-05-09 15:38:52 +0300

    Добавит данные в таблицы

---
+++ sql/07-insert-employers-data.sql
@@ -0,0 +1,8 @@
+INSERT INTO employers 
+VALUES ('Компания 1', 'Адрес 1', 0.8, TRUE);
+
+INSERT INTO employers 
+VALUES ('Компания 2', 'Адрес 2', 1.8, TRUE);
+
+INSERT INTO employers 
+VALUES ('Компания 3', 'Адрес 3', 3.8, FALSE);
---
+++ sql/08-insert-conversation-data.sql
@@ -0,0 +1,2 @@
+INSERT INTO "conversation" 
+VALUES ('Denis Pepper', 'Компания 1', 'Hello, people!', '2024-05-01 16:30:00');
