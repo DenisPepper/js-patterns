b18cc47dfc493c72d31faeb90ae13d8266cbf571
DenisPepper <thech@yandex.ru>
2024-05-10 15:37:27 +0300

    Добавит уникальный идентификатор с автоувеличением

--- sql/02-create-table.sql
+++ sql/02-create-table.sql
@@ -2,7 +2,7 @@
 CREATE TYPE employment_status AS ENUM('employed', 'self-employed', 'unemployed');
 
 CREATE TABLE users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY,
     full_name VARCHAR(150),
     salary INT,
     current_status employment_status
