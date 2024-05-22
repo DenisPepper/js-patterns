1b636ebcdb1976093c27445fda7118439d843151
DenisPepper <thech@yandex.ru>
2024-05-22 20:14:35 +0300

    Добавит CRUD операции

--- mongo/playground-1.mongodb.js
+++ mongo/playground-1.mongodb.js
@@ -34,3 +34,53 @@ db.products.find()
 db.products.find({})
 db.products.find().pretty()
 */
+
+
+// CRUD operations
+
+// CREATE
+// insertOne(data, options)
+// insertMany(data, options)
+/*
+   insertMany(
+    [
+        {
+           key: "value",
+           key1: "value1",
+        },
+        {
+           key: "value",
+           key1: "value1",
+        }
+    ]
+   )
+*/
+
+// READ
+// find(filter, options)
+/*
+   find({key: "value"})
+   find({size: {$gt: 10}}) -больше чем 10
+*/
+// findOne(filter, options)
+
+// UPDATE
+// updateOne(filter, data, options)
+/*
+   updateOne(name: "Denis", {$set: {key: value}}) -обновит документ по фильтру
+   updateMany({}, {$set: {key: value}})           -обновит все документы (если такого ключа <key> нет, то он будет добавлен)
+
+   Оператор $set заменяет значение поля на указанное значение.
+*/
+
+
+
+// updateMany(filter, data, options)
+
+//DELETE
+// deleteOne(filter, options)
+/*
+   deleteOne({key: "value"})
+*/
+
+// deleteMany(filter, options)
