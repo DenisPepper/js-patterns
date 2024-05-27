6d4afcdec5b4e742b7358352c9f992cd6218e481
DenisPepper <thech@yandex.ru>
2024-05-27 20:56:54 +0300

    Добавит документ

--- mongo/playground-2.mongodb.js
+++ mongo/playground-2.mongodb.js
@@ -4,4 +4,7 @@ const PRODUCTS = 'products';
 // Create a new database.
 use(DATABASE);
 
-db.createCollection(PRODUCTS)
+//db.createCollection(PRODUCTS)
+
+
+db.products.insertOne({furnType: 'Комод', price: 12.99});
