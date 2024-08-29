2ff7e319acd6a94b244a3011702d4156f05c0fb5
DenisPepper <thech@yandex.ru>
2024-08-29 11:57:11 +0300

    Добавит перехватчик на set

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -14,6 +14,15 @@ const handler = {
     const value = target[key];
     return typeof value === 'function' ? value.bind(target) : value;
   },
+
+  set(target, prop, value) {
+    if (!target[key]) return;
+
+    if (key.startsWith('_')) throw new Error(`Свойство ${key} не найдено!`);
+
+    target[key] = value;
+    return true;
+  },
 };
 
 const proxy = new Proxy(target, handler);
