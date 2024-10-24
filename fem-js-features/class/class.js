afea9c3fcdfb35f51ae35d2dfd2041ced94dfc01
DenisPepper <thech@yandex.ru>
2024-10-25 00:19:37 +0300

    Добавит примеры синтаксиса

--- fem-js-features/class/class.js
+++ fem-js-features/class/class.js
@@ -1,6 +1,10 @@
 export class Clss {
+  static {
+    // блок инициализации
+    // будет аыполнен один раз
+  }
+
   constructor() {
-     
     // список аргументов обычной функции
     console.log(arguments[0]);
     console.log(arguments[1]);
@@ -9,3 +13,31 @@ export class Clss {
 }
 
 const cls = new Clss(1, 'two', true);
+
+// кросплатформенный глобальный объект
+globalThis;
+
+// необязательная переменная error в catch
+try {
+} catch {}
+
+// преобразование функции в строку и выполнение в другом контексте
+function example(arg) {
+  console.log(`Examle here with ${arg}`);
+}
+const ex = example.toString();
+const exampleFromString = Function(`return ${ex}`)();
+exampleFromString('once arg');
+
+// короткий синтаксис логическиъ операторов
+let a = 1;
+a &&= 8; // a = a && 2
+
+let b = null;
+b ||= 2; // b = b || 2
+
+let c = undefined;
+c ??= 4; // c = c ?? 4
+
+// возведение в степень
+const pow = 2 ** 3; // 2^3
