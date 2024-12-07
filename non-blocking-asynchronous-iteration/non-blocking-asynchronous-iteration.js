1aa6b2a899857601c955ff7a6e2a8009451fa477
DenisPepper <thech@yandex.ru>
2024-12-07 14:02:47 +0300

    Добавит интервал прерывания цикла

--- non-blocking-asynchronous-iteration/non-blocking-asynchronous-iteration.js
+++ non-blocking-asynchronous-iteration/non-blocking-asynchronous-iteration.js
@@ -2,7 +2,8 @@
 import { printTimeDiff } from './time-diff.js';
 import { runTask } from './task.js';
 
-const numbers = new Array(100).fill(1);
+const numbers = new Array(1000).fill(1);
+const INTERVAL = 8;
 
 const forEach = (array, fn) => {
   let index = 0;
@@ -18,11 +19,31 @@ const forEach = (array, fn) => {
   next(index);
 };
 
-const interval = 10;
-const taskId = runTask(interval);
+const each = (array, fn) => {
+  let index = 0;
+  const maxIndex = array.length - 1;
+  let startTime = Date.now();
+
+  const next = () => {
+    while (index <= maxIndex) {
+      const time = Date.now();
+      const diff = time - startTime;
+      if (diff > INTERVAL) {
+        startTime = time;
+        setTimeout(next, 0);
+        break;
+      }
+      fn(array[index], index++);
+    }
+  };
+
+  next(index);
+};
+
+const taskId = runTask(INTERVAL);
 const startTime = process.hrtime.bigint();
 
-forEach(numbers, (item, index) => {
+each(numbers, (item, index) => {
   console.log(index);
   if (index === numbers.length - 1) {
     clearInterval(taskId);
