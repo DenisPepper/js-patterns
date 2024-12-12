4301fdd1b5c7ee6eb6521c43c5cd66f56c90c572
DenisPepper <thech@yandex.ru>
2024-12-12 23:31:55 +0300

    Добавит пример последовательного извлечения данных с помощью генератора

---
+++ equential-fetching-data/generator.js
@@ -0,0 +1,33 @@
+// https://habr.com/ru/articles/490524/
+// последовательная отправка запросов,
+// где параметр следующего запроса, является результатом ответа на предыдущий запрос
+// реализация через генератор
+import { fetchData } from './fetch.js';
+
+const urls = [
+  'https://jsonplaceholder.typicode.com/todos/',
+  'https://jsonplaceholder.typicode.com/todos/',
+  'https://jsonplaceholder.typicode.com/todos/',
+  'https://jsonplaceholder.typicode.com/todos/',
+];
+
+function* gen(urls, arg) {
+  let result = arg;
+  for (const url of urls) {
+    result = yield fetchData(`${url}${result}`);
+  }
+}
+
+const INITIAL = '1';
+
+const getData = (getters, arg) => {
+  const result = getters.next(arg);
+  if (!result.done) {
+    result.value.then((data) => {
+      console.log(data.id, data.title);
+      getData(getters, (data.id += 1));
+    });
+  }
+};
+
+getData(gen(urls, INITIAL));
