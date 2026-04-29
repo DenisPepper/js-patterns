1de9b70e3eac3775acdcc9eedce1514ccb6e1bc8
DenisPepper <thech@yandex.ru>
2026-04-29 21:09:38 +0300

    обновит методы DAG

--- dag/make-levels.js
+++ dag/make-levels.js
@@ -41,3 +41,20 @@ export class LevelBuilder {
         this.#addLevel(next);
     }
 }
+
+const data = [
+    { name: "J", prev: ["E", "F", "G", "I", "H"], next: [] },
+    { name: "E", prev: ["A"], next: ["J"] },
+    { name: "F", prev: ["A"], next: ["J"] },
+    { name: "G", prev: ["A"], next: ["J"] },
+    { name: "I", prev: ["A"], next: ["J"] },
+    { name: "H", prev: ["A", "B", "C"], next: ["J"] },
+    { name: "A", prev: ["D"], next: ["E", "F", "G", "I", "H"] },
+    { name: "B", prev: [], next: ["H"] },
+    { name: "C", prev: [], next: ["H"] },
+    { name: "D", prev: [], next: ["A"] },
+];
+
+const levels = new LevelBuilder(data);
+
+console.dir(levels);
---
+++ dag/transform-deps-to-data.js
@@ -0,0 +1,66 @@
+// преобразователь двумерного массива работ в одномерный массив событий
+// событие представляет собой элемент двусвязанного списка
+// { name: "effect", prev: [], next: [] }
+
+function create() {
+    const effect = { prev: [], next: [] };
+    return effect;
+}
+
+function add(job, store) {
+    const [prevName, nextName] = job;
+
+    // для начального эффекта нужно заполнить массив next
+    if (store.has(prevName)) {
+        const effect = store.get(prevName);
+        effect.next.push(nextName);
+    } else {
+        const effect = create();
+        effect.next.push(nextName);
+        store.set(prevName, effect);
+    }
+
+    // для конечного эффекта нужно заполнить массив prev
+    if (store.has(nextName)) {
+        const effect = store.get(nextName);
+        effect.prev.push(prevName);
+    } else {
+        const effect = create();
+        effect.prev.push(prevName);
+        store.set(nextName, effect);
+    }
+}
+
+function transform(jobs) {
+    // нужен store для хранения эффектов-объектов (событий/результатов)
+    const store = new Map();
+
+    // массив вида ["D", "A"] указывает на: "D" - начальный эффект, "A" - конечный эффект
+    for (const job of jobs) {
+        // создаем новые эффекты-объекты для начального и конечного имен эффектов
+        add(job, store);
+    }
+
+    return Array.from(store, ([name, effect]) => {
+        return { name, prev: effect.prev, next: effect.next };
+    });
+}
+
+const dependencies = [
+    ["D", "A"], // D → A Собрать раму велосипеда
+    ["A", "E"], // A → E Установить руль
+    ["A", "F"], // A → F Установить сиденье
+    ["A", "G"], // A → G Установить тормоза
+    ["A", "I"], // A → I Установить трансмиссию
+    ["B", "H"], // B → H Установить колёса на раму
+    ["C", "H"], // C → H Установить колёса на раму
+    ["A", "H"], // A → H Установить колёса на раму
+    ["E", "J"], // E → J Проверить качество сборки
+    ["F", "J"], // F → J Проверить качество сборки
+    ["G", "J"], // G → J Проверить качество сборки
+    ["H", "J"], // H → J Проверить качество сборки
+    ["I", "J"], // I → J Проверить качество сборки
+];
+
+const data = transform(dependencies);
+console.dir(data, { depth: null });
